43f24988583f5d1f67e21471c77b7441e715eeaf
Neamul01 <neamulhaque2002@gmail.com>
2022-04-21 14:22:46 +0600

    steps added

---
+++ __steps.js
@@ -0,0 +1,26 @@
+/**
+ * ---------------------------------
+ * Basic server setup
+ * ---------------------------------
+ * 1. create a folder(manually or mkdir)
+ * 2. Open command line to that folder.
+ * 3. npm init
+ * 4. npm install express
+ * 5. create index.js
+ * ----------------------------------
+ * For Express/node server
+ * ----------------------------------
+ * 1. require express
+ * 2. Create app variable
+ * 3. Declare port
+ * 4. set app.get('/)
+ * 5. listen to port
+ * 6. node index.js
+ * 7. check browser for that port
+ * ----------------------------------
+ * get
+ * ----------------------------------
+ * post
+ * ----------------------------------
+ * 1
+ */
